193f9ae19bc5df315e9ef142570812fc26b0b173
Zhandos <saginov-2001@mail.ru>
2018-06-21 18:18:46 +0300

    adaptive

--- main.php
+++ main.php
@@ -4,25 +4,31 @@
 		<div class="grid-container">
   			<div class="content">
   				<div class="news">
-            <div class="news-head">
-              <div class="news-img">
-                <img src="img/header.jpg" alt=""> 
-              </div>
-              <div class="news-title">
-                Lorem ipsum dolor sit amet.    
-              </div>
+            		<div class="news-head">
+              			<div class="news-img">
+                			<img src="img/header.jpg" alt=""> 
+              			</div>
+              			<div class="news-title">
+                			Lorem ipsum dolor sit amet.    
+              			</div>
+            		</div>
+  					<span class="info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos perspiciatis maxime maiores ducimus a amet, repudiandae ipsa dolores? Architecto saepe soluta odit non obcaecati quaerat earum enim fuga reprehenderit facere.</span>
+            		<div class="readmore">Read More</div>
+            	</div>
+            	<div class="news">
+            		<div class="news-head">
+              			<div class="news-img">
+                			<img src="img/header.jpg" alt="" class="news-title-img" /> 
+              			</div>
+              			<div class="news-title">
+                			Lorem ipsum dolor sit amet.    
+              			</div>
+            		</div>
+  					<span class="info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos perspiciatis maxime maiores ducimus a amet, repudiandae ipsa dolores? Architecto saepe soluta odit non obcaecati quaerat earum enim fuga reprehenderit facere.</span>
+            		<div class="readmore">Read More</div>
+            	</div>
             </div>
-  					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos perspiciatis maxime maiores ducimus a amet, repudiandae ipsa dolores? Architecto saepe soluta odit non obcaecati quaerat earum enim fuga reprehenderit facere.
-            <div class="readmore">Read More</div>
-            </div>
-  					<div class="news">Neque nostrum, sint reiciendis quod minima asperiores accusamus maxime quibusdam dignissimos, dolor minus commodi dicta voluptatum et, in delectus tempora voluptate vel. Soluta perspiciatis consequatur dolore tempore modi non amet!
-            </div>
-  					<div class="news">Odio id laboriosam maxime consectetur illum iure nobis at corrupti. Dicta commodi sed nisi vitae accusamus illum aperiam quaerat quidem laborum minus assumenda eos et, qui inventore distinctio, minima? Ea!</div>
-  					<div class="news">Totam explicabo asperiores magnam quaerat laudantium quam consequuntur expedita, qui voluptates dolorem voluptatibus itaque dolores laboriosam exercitationem velit beatae laborum placeat! Harum a quam reprehenderit tempore esse saepe repellat ea.
-            </div>
-  					<div class="news">Earum fugit vel sapiente temporibus, blanditiis dicta veniam praesentium. Totam numquam molestias, similique eius in soluta culpa perspiciatis. Quasi debitis accusamus veritatis ratione nihil impedit ipsam obcaecati molestias porro accusantium!</div>
-  					 				
-			</div>
+  					
   			<div class="additional">
   	  			sadas
 			</div>
