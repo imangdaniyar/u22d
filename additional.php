5295a7be29cca6bd3e54dc808299dd04b3f127b3
Zhandos <saginov-2001@mail.ru>
2018-06-26 16:49:56 +0300

    1324

--- additional.php
+++ additional.php
@@ -3,33 +3,16 @@
 	if(isset($_SESSION['logged'])) :?>
  	<?php 
  		$user = $_SESSION['logged'];
- 		$usera = R::findOne('users' , 'id = ?', array($user['id']));
+ 		$usera = R::findOne('users' , 'id = ?', array($user->id));
  		$_SESSION['logged'] = $usera;
  		$user = $_SESSION['logged'];
  		
+ 		
  	?>
-<<<<<<< HEAD
- 	<div class="mini-profile">
- 		<div class="mp-avatar">
- 			<img src="avatars/<?php echo $user->avatar?>" alt="avatar" class="mp-ava">
- 		</div>
- 		<div class="mp-data">
- 			<div class="mp-info">
- 				<?php echo $user->name.' '.$user->sname ?>
- 			</div>
- 			<div class="mp-status">
- 				<div contenteditable="true" id="status" ><?php 
- 				if($_SESSION['logged']->status != ''){
- 					echo $_SESSION['logged']->status;	
- 				} else echo 'Изменить статус';
- 				
-
- 				?></div>
- 			</div>
- 		</div>
-=======
  	<div class="section" style="perspective: 500px;">
  		<div class="mini-profile">
+ 			<div class="zac">
+ 			<img style="width: 100%; height: 100%;" src="img/zac.png" /></div>
 	 		<div class="mp-avatar">
 	 			<img src="avatars/<?php echo $user->avatar?>" alt="avatar" class="mp-ava">
 	 		</div>
@@ -38,13 +21,18 @@
 	 				<?php echo $user->name.' '.$user->sname ?>
 	 			</div>
 	 			<div class="mp-status">
-	 				<input type="textarea" name="status" placeholder="Нет статуса" rows="auto" id="status" dir="rtl" value="<?php echo $_SESSION['logged']->status ?>">
-	 			</div>
+ 				<div contenteditable="true" id="status" ><?php 
+ 				if($_SESSION['logged']->status != ''){
+ 					echo $_SESSION['logged']->status;	
+ 				} else echo 'Изменить статус';
+ 				?>
+
+ 				</div>
+ 			</div>
 	 		</div>
+	 		<a href="profile.php?id=<?php echo $user->id ?>"><div class="refer-profile">Перейти в профиль</div></a>
 	 		<hr>
 	 	</div>
-
->>>>>>> d80e4e309fe75d3e528850492a1f40e416aa04c3
  	</div>
  	
 
