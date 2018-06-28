b5e75ddb3ecad3781cda774eb76bb726487c24b4
Zhandos <saginov-2001@mail.ru>
2018-06-29 02:08:23 +0300

    xcdfghjk

--- new.php
+++ new.php
@@ -9,53 +9,96 @@
 ?>
 		<div class="grid-container">
   			<div class="content">
-  				<?php 
-            echo('<div class="news">
-                    <div class="news-head">
-                      <div class="news-img">
-                        <img src="img/header.jpg" alt=""> 
-                      </div>
+  				<div class="new-container">
+                    
+                    <div class="title-layout">
+                      <div class="new-title">
+                        Lorem ipssdfailhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh fdsaf asdf sadfhasi
 
-                      <div class="news-title">
-                        '.$new->title.'    
-                      </div>
+                      <div class="additional-info">
+                          Saginov Zhandos 10 101 01110 101
+                      </div>   
                     </div>
-
+                  </div>
                     <div id="block-for-slider">
-        <div id="viewport">
-            <ul id="slidewrapper">
-                <li class="slide">
-                  <img src="img/modal-bg.jpg" alt="1" class="slide-img">
-                </li>
-                <li class="slide">
-                  <img src="img/bg.jpg" alt="2" class="slide-img">
-                </li>
-                <li class="slide">
-                  <img src="img/modal-bg.jpg" alt="3" class="slide-img">
-                </li>
-                <li class="slide">
-                  <img src="img/gray-bg.jpg" alt="4" class="slide-img">
-                  </li>
-            </ul>
+                      <div id="viewport">
+                        <ul id="slidewrapper">
+                         <li class="slide">
+                            <img src="img/modal-bg.jpg" alt="1" class="slide-img">
+                         </li>
+                         <li class="slide">
+                            <img src="img/bg.jpg" alt="2" class="slide-img">
+                         </li>
+                         <li class="slide">
+                            <img src="img/modal-bg.jpg" alt="3" class="slide-img">
+                         </li>
+                         <li class="slide">
+                            <img src="img/gray-bg.jpg" alt="4" class="slide-img">
+                         </li>
+                        </ul>
 
             
-        </div>
-    </div>
-
-                <div class="info">
-                '.$new->text.'</div>
-                    <div class="readmore-container">
-                  <div class="comments"><span style="left: 50%;" class="center">0 Комментариев</span></div>
-                  <div class="avtor"><span class="center">'.$tu_sname.' '.$tu_name.' '.$new->date.'</span></div>
-                      
-
                     </div>
+                  </div>
+
+              <div class="new-text">
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem saepe architecto iusto consequuntur, amet beatae soluta expedita eum quas consequatur quos. Voluptas quia similique deserunt ipsam qui, odio alias, soluta!</div>
+                <div>Voluptatibus architecto unde voluptas doloribus ratione repellendus. Aliquid sint, modi molestias, voluptatem, facilis, maiores perspiciatis iusto itaque ducimus expedita maxime. Nam exercitationem minus dolore deserunt, asperiores autem! Eaque, nisi, molestias?</div>
+                <div>Hic inventore dolores quisquam, in facilis optio quidem consectetur cum odio consequuntur earum unde voluptate est quibusdam incidunt repellat quis aliquam totam. Possimus autem, totam veritatis distinctio doloremque adipisci architecto.</div>
+              </div>
+                
 
                   
                   </div>
-                  <hr>
-              ');
-           ?>
+                  <div class="comment-container">
+                    <div class="make-comment">
+                      <div class="button-box-comments">
+                        <button class="com-buttons" type="file"><i dir="rtl" class="fa fa-paperclip"  aria-hidden="true"></i> Прикрепить</button>
+                        <button class="com-buttons" type="button"><i class="fa fa-share" aria-hidden="true"></i> Отправить</button>
+                      </div>
+                      <label style="font-size: 1.3vw; " for="comarea">Оставьте комментарий:</label>
+                      <div contenteditable="true" name="comarea" class="comment-box"></div>
+                    </div>
+                    <br>
+                    <br>
+                    
+                    <div class="comment">
+                      <div class="comments-ava">
+                        <img src="avatars/default_avatar.jpg" alt="ava" class="com-ava">
+                      </div>
+                      <div class="com-box">
+                        <div class="comment-text">
+                          <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus asperiores provident ut in sequi, corporis magni facere totam ex earum, ea quod reiciendis laborum sunt. Fugit, distinctio quaerat quis ea.</div>
+                         
+                        </div>
+                        <br class="clear">
+                        <div class="com-info">
+                        fsdfsdfsdfds
+                        </div>
+                      </div>
+                    </div>
+                     
+                    <hr>
+                    <div class="comment">
+                      <div class="comments-ava">
+                        <img src="avatars/default_avatar.jpg" alt="ava" class="com-ava">
+                      </div>
+                      <div class="com-box">
+                        <div class="comment-text">
+                          <div>Lorem ipsum dolor sit amet.</div>
+                         
+                        </div>
+                        <br class="clear">
+                        <div class="com-info">
+                        fsdfsdfsdfds
+                        </div>
+                      </div>
+                    </div>
+                    
+                    <hr>
+
+                  </div>
+                  
           
   			</div>		
   			<?php 
