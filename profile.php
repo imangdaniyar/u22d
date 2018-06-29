13636216a07f427c6f3ee10bb6b98a787a6f7e8c
Daniyar Imangaliyev <imangdaniyar@gmail.com>
2018-06-29 15:52:55 +0500

    qwertyu

--- profile.php
+++ profile.php
@@ -1,10 +1,57 @@
 <?php 
 	include 'header.php';
+
 ?>
 		<div class="grid-container">
   			<div class="content">
+
   				<div class="profile-container">
+            <div class="profile-head">
+              <div class="profile-avatar">
+                <img src="img/ava.png" alt="" class="ava">
+              </div>
+              <div class="profile-info">
+                <div class="name">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, reiciendis.</div>
+              </div>
+            </div>
+
+            <div class="profile-content">
+              <div class="nav-head">
+                <div class="profile-nav">
+                  <ul class="profile-navigation">
+                    <li class="information active">О себе</li>
+                    <li class="information">Фотографии</li>
+                  </ul>
+                  <hr>  
+                </div>
+              </div>
+              <div class="personal">
+                <div class="personal-info about">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae sit ab aspernatur deserunt laborum magnam, dolore optio ex iure, mollitia eius consequuntur fugiat esse quod minima est labore inventore quae, voluptates itaque ut. Amet nostrum fugiat aperiam culpa ea. Dignissimos minima, laboriosam tempora non eveniet velit incidunt consequatur tenetur nostrum eligendi doloremque quibusdam sunt sed excepturi molestiae nisi ex rem quidem, mollitia nobis sit ipsa atque? Molestias delectus sequi facilis, illum maxime eius quas cum fugiat eveniet in. Quas laboriosam veniam harum quod illum vitae, voluptate officia doloremque officiis dolorum aut eum architecto. Totam tempore pariatur eum voluptates in porro?                  
+                </div>
+                <div class="personal-info photos">
+                  <div class="photos_container">
+                    <div class="photo-container">
+                      <img src="img/modal-bg.jpg" alt="" class="photo">
+                    </div>
+                    <div class="photo-container">
+                      <img src="img/modal-bg.jpg" alt="" class="photo">
+                    </div>
+                    <div class="photo-container">
+                      <img src="img/modal-bg.jpg" alt="" class="photo">
+                    </div>
+                    <div class="photo-container">
+                      <img src="img/modal-bg.jpg" alt="" class="photo">
+                    </div>
+                     <div class="photo-container">
+                      <img src="img/modal-bg.jpg" alt="" class="photo">
+                    </div>  
+                  </div>
+                </div>
                 
+              </div>
+
+            </div>
           </div>
         </div>
   					
